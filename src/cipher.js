c9764ef3732bcc7d4cd1f42020bec752d8370b01
Judith <baruki110214@gmail.com>
2018-06-13 18:41:31 -0500

    cambios3

--- src/cipher.js
+++ src/cipher.js
@@ -1,11 +1,11 @@
+let  posicion=33;
+
 debugger;
 window.cipher = {
-let  posicion=33;
-let  str="";
-let resultado="";
-let code=resultado;
   //declaramos la primera funcion para encriptar
-  cipher: (str,posicion) => {
+  cipher: (str) => {
+    let result="";
+    let code=result;
       //utilizamos un loop para validar que letra ingreso como mensaje y encripta segun codigo ASCII
       for (let i=0; i<str.length; i++) {
              //se genera un for para el caso en que las letras son mayusculas
@@ -32,29 +32,31 @@ let code=resultado;
 
    },
     //declaramos la segunda funciòn para descifrar
-descipher:(str,posicion) =>{
+descipher:(str2) =>{
+  let result2="";
+  let code2=result2;
     //utilizamos un loop para validar que letra ingreso como mensaje y encripta segun codigo ASCII
-    for (let i=0; i<str.length; i++) {
+    for (let i=0; i<str2.length; i++) {
            //se genera un for para el caso en que las letras son mayusculas
-           if (str.charCodeAt(i)>=65 && str.charCodeAt(i)<=90){
-             code = (((str.charCodeAt(i)-65)+posicion)%26)+65;
+           if (str2.charCodeAt(i)>=65 && str2.charCodeAt(i)<=90){
+             code2 = (((str2.charCodeAt(i)-65)+posicion)%26)+65;
            console.log(code); }
 
            //se genera un for para el caso en que las letras son minusculas
-           else if (str.charCodeAt(i)>=97 && str.charCodeAt(i)<=122){
-             code = (((str.charCodeAt(i)-97)+posicion)%26)+97;
+           else if (str2.charCodeAt(i)>=97 && str2.charCodeAt(i)<=122){
+             code2 = (((str2.charCodeAt(i)-97)+posicion)%26)+97;
            console.log(code);}
            //se genera un for para el caso en que hay un espacio
-           else if (str.charCodeAt(i)===32){
-             code = 32;}
+           else if (str2.charCodeAt(i)===32){
+             code2 = 32;}
            //se genera un for para el caso en que hay caracteres especiales
-           else if (str.charCodeAt(i)>=32 && str.charCodeAt(i)<=64){
-             code = (((str.charCodeAt(i) - 32)+posicion) % 26) + 32;
+           else if (str2.charCodeAt(i)>=32 && str2.charCodeAt(i)<=64){
+             code2 = (((str2.charCodeAt(i) - 32)+posicion) % 26) + 32;
            console.log(code);}
 
-           result+=String.fromCharCode(code);
+           result2+=String.fromCharCode(code2);
          }
           //result
-          return result;
+          return result2;
  }
 }
--- src/index.js
+++ src/index.js
@@ -2,25 +2,22 @@
 let message=document.getElementById("message");
 //creamos una variable para llamar el string ingresado para descifrar
 let newMessage=document.getElementById("newMessage");
-let boton=document.getElementById("encriptar");
+let boton=document.getElementById("buttoncipher");
 let print=document.getElementById("encrypt");
 let boton2=document.getElementById("buttondescipher");
-
-//declaramos una variable para almacenar el string retornado al encriptar y otra para poder trabajarlo como number
-//let result= "";
-//let code = result;
-//let result2= "";
-//let code2 = result2;
-
+let print2=document.getElementById("decrypt");
+//declaramos una funcion para imprimir resultado de cipher
+function encriptar(){
+  let mensaje=message.value;
+  print.innerHTML=window.cipher.cipher(mensaje);
+}
+//declaramos una funcion para imprimir resultado de descipher
+function descifrar(){
+  let nuevoMensaje=newMessage.value;
+  let imprime1=window.cipher.descipher(newMessage);
+  document.getElementById('encrypt').innerHTML=imprime1;
+}
 //declaramos una variable para almacenar el string retornado al encriptar y otra para poder trabajarlo como number
 boton.addEventListener("click",encriptar);
 //creamos una variable para llamar al boton2 descifrar
-boton2.addEventListener("click",printDecryptedText);
-
-function encriptar(){
-  encrypt.innerHTML=window.cipher.cipher(resultado);
-}
-
-function printDecryptedText(){
-  alert("funciona");
-}
+boton2.addEventListener("click",descifrar);
