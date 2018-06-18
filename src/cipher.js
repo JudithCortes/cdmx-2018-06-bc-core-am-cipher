a8ddfa68bfe4606af80d5db08985959cae51a65a
Judith <baruki110214@gmail.com>
2018-06-17 21:27:19 -0500

    actualizaciones 17 de Junio

--- src/cipher.js
+++ src/cipher.js
@@ -1,64 +1,83 @@
 //se declara un offset fijo
 let offset=33;
+let  position=offset+26;
 
 //debugger;
 //se crea objeto contenedor de dos elementos funcion
 window.cipher = {
   //declaramos la primera funcion para encriptar con un parametro "string"
-  cipher:  => {
+  encode: (str) =>{
 
     //se declara una variable donde se guardarà el resultado de la operacion
-
+    let result="";
     let code=result;
       //utilizamos un loop para validar que letra ingreso como mensaje y encripta segun codigo ASCII
       for (let i=0; i<str.length; i++) {
 
           let num=str.charCodeAt(i);
+          let a=num-65;
+          let b=num-97;
+          let c=num-33;
 
             //se genera un for para el caso en que las letras son mayusculas
             if (num>=65 && num<=90){
               code = ((a+position)%26)+65;
-              console.log(code);
              }
 
              //se genera un for para el caso en que las letras son minusculas
              else if (num>=97 && num<=122){
                code = ((b+position)%26)+97;
-               console.log(code);
              }
+             //se genera un for para el caso en que hay un espacio
+             else if (num===32){
+               code = 32;}
+             //se genera un for para el caso en que hay caracteres especiales
+             else if (num>=33 && num<=64){
+               code = ((c+position) % 26) + 33;
+            }
 
              result+=String.fromCharCode(code);
            }
             //result
      return result;
-
    },
+
+
     //declaramos la segunda funciòn para descifrar
-descipher:(str2) =>{
+decode: (str2)=>{
   //se declara una variable donde se guardarà el resultado de la operacion
   let result2="";
   let code2=result2;
     //utilizamos un loop para validar que letra ingreso como mensaje y encripta segun codigo ASCII
     for (let i=0; i<str2.length; i++) {
 
-
+        let num=str2.charCodeAt(i);
+        let a=num+65;
+        let b=num+97;
+        let c=num-32;
 
           //se genera un for para el caso en que las letras son mayusculas
           if (num>=65 && num<=90){
             code2 = ((a-position)%26)+65;
-            console.log(code2);
            }
 
            //se genera un for para el caso en que las letras son minusculas
            else if (num>=97 && num<=122){
              code2 = ((b+position)%26)+97;
-             console.log(code2);
            }
-           
+           //se genera un for para el caso en que hay un espacio
+           else if (num===32){
+             code2 = 32;}
+           //se genera un for para el caso en que hay caracteres especiales
+           else if (num>=32 && num<=64){
+          //code2 = ((c-position) % 26) + 33;
+          code2 = ((c+position) % 26) + 32;
+          }
 
            result2+=String.fromCharCode(code2);
          }
           //result
    return result2;
 }
+
 }
--- src/index.js
+++ src/index.js
@@ -8,18 +8,23 @@ let boton2=document.getElementById("buttondescipher");
 const encryptMessage = () =>{
   //declaramos variable dentro de la funcion para guardar el valor "string" ingresado por el usuario
   let message=document.getElementById("message").value;
+  //declaramos una variable para almacenar el resultado retornado del elemento funcion cipher del objeto window.cipher
+ let print1=window.cipher.encode(message);
   //imprimimos en la variable print el resultado de la funcion elemento del objeto window.cipher con parametro mensaje
   document.getElementById("encrypt").innerHTML=print1;
 }
 
-//declaramos una funcion para imprimir resultado de descipher
+//declaramos una funcio
+//n para imprimir resultado de descipher
 const decryptMessage = () =>{
   //declaramos variable para almacenar el valor "string" ingresado por el usuario
   let newMessage=document.getElementById("newMessage").value;
-
+  //declaramos una variable para almacenar el resultado retornado del elemento funcion descipher del objeto window.cipher
+  let print2=window.cipher.decode(newMessage);
   //se imprime en "encrypt" el valor de la variable imprime1
   document.getElementById("decrypt").innerHTML=print2;
 }
+//creamos una variable para llamar al botòn1 cifrar
 boton.addEventListener("click",encryptMessage);
 //creamos una variable para llamar al boton2 descifrar
 boton2.addEventListener("click",decryptMessage);
